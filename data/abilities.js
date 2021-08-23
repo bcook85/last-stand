74e824fe4c86e5e9457577b316a07cc80b5f4659
Bryan Cook <bryandeancook@gmail.com>
2021-08-22 19:57:05 -0600

    abilities.js Added basic info for loading purposes

--- data/abilities.js
+++ data/abilities.js
@@ -1,18 +1,26 @@
 const PLAYER_ABILITIES = [
   [// 0 for PLAYER_TYPES[0]
     {//0
+      "name": "&#34;Coming soon!&#34;"
+      ,"description": "... eventually"
     }
   ]
   ,[// 1 for PLAYER_TYPES[1]
     {//0
+      "name": "&#34;Coming soon!&#34;"
+      ,"description": "... eventually"
     }
   ]
   ,[// 2 for PLAYER_TYPES[2]
     {//0
+      "name": "&#34;Coming soon!&#34;"
+      ,"description": "... eventually"
     }
   ]
   ,[// 3 for PLAYER_TYPES[3]
     {//0
+      "name": "&#34;Coming soon!&#34;"
+      ,"description": "... eventually"
     }
   ]
 ];
